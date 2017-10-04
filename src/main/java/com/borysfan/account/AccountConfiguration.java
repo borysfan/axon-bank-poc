42641c171e9857ff8e01727914a1772bd04caf68
e-dkbs <dominik.bartos@gft.com>
2017-10-04 16:15:57 +0200

    Sending events to RabbitMQ

--- src/main/java/com/borysfan/account/AccountConfiguration.java
+++ src/main/java/com/borysfan/account/AccountConfiguration.java
@@ -4,6 +4,8 @@ import com.mongodb.MongoClient;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.mongo.eventsourcing.eventstore.DefaultMongoTemplate;
 import org.axonframework.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +15,27 @@ public class AccountConfiguration {
     @Bean
     public EventStorageEngine eventStore(MongoClient mongoClient) {
         return new MongoEventStorageEngine(new DefaultMongoTemplate(mongoClient));
+    }
+
+    @Bean
+    public Exchange exchange() {
+        return ExchangeBuilder.fanoutExchange("BankEvents").build();
+    }
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable("BankEvents").build();
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with("*").noargs();
+    }
+
+    @Autowired
+    public void configure(AmqpAdmin amqpAdmin) {
+        amqpAdmin.declareExchange(exchange());
+        amqpAdmin.declareQueue(queue());
+        amqpAdmin.declareBinding(binding());
     }
 }
