96089c552e3844586ed5faa76a5af4514d608318
dbartos <borysfan@gmail.com>
2017-10-22 19:31:13 +0200

    New processing group.

---
+++ axon-bank/src/main/java/com/borysfan/reply/EchoHandler.java
@@ -0,0 +1,24 @@
+package com.borysfan.reply;
+
+import com.borysfan.core.api.MoneyDepositedEvent;
+import com.borysfan.core.api.MoneyWithdrawnEvent;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@ProcessingGroup("echo")
+@Component
+public class EchoHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventHandler.class);
+
+    @EventHandler
+    public void on (MoneyDepositedEvent event) {
+        LOGGER.info("Money deposited {}", event.getAmount());
+    }
+
+    public void on(MoneyWithdrawnEvent event) {
+        LOGGER.info("Money withdrawn {}", event.getAmount());
+    }
+}
